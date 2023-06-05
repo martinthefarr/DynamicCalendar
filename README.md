dba69dda38c5faf2cd8fdb61c74a160366c7254c
Martin Farr <martinfarr24@gmail.com>
2023-06-05 08:46:06 -0500

    Started a readme but still needs to be finished with a picture. Deleted some green text to clean up the code.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # DynamicCalendar
+A work-day-planner that allows the user to save data in an hourly section of the workday.
+
+## About
+An aplication that uses: HTML, CSS, JavaScript, BootStrap, JQuery, and DayJS. Used to plan work goals, by the hour, to maximize time management. 
+
+## User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+## GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+## Visuals
+
+## Link
--- jss/script.js
+++ jss/script.js
@@ -1,69 +1,41 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 var timeblocks = $('#timeblocks');
+var hours = dayjs().hour()
 
 var today = dayjs();
+
+
 $('#currentDay').text(today.format('MMM D, YYYY'));
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  timeblocks.on('click', function () {
+  $(".saveBtn").on("click", function(){
+    console.log($(this).parent().attr("id"))
+    var saved = ($(this).siblings("textarea").val())
+    var id = ($(this).parent().attr("id"))
+    localStorage.setItem(id, saved)
+  })
+
+
+  $(".time-block").each(function () {
     // The background will be Grey
-    if (isPast) {
-      $('body').css({ 'background-color': '#ff6961', color: '#d3d3d3' });
-
-      // isDark = !isDark;// The background will be Red
-    } else if (isPresent){
-      $('body').css({ 'background-color': '#ff6961', color: '#white' });
-
+    // console.log(parseInt($(this).attr("id").slice(5)))
+    var timeOfBlocks = (parseInt($(this).attr("id").slice(5)))
+    // console.log(hours)
+    if (timeOfBlocks < hours) {
+      $(this).addClass("past").removeClass("future present")
+      // The background will be Red
+    } else if (timeOfBlocks === hours){
+      $(this).addClass("present").removeClass("future past")
       // The background will be Green
-    } else (isFuture) {
-      $('body').css({ 'background-color': '#d3d3d3', color: '#white'});
-
-
+    } else {
+      $(this).addClass("future").removeClass("present past")
     }
   });
 
-
-
-
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  $(".time-block").each(function (){
+    var fetch = $(this).attr("id")
+    $(this).children("textarea").val(localStorage.getItem(fetch)) 
+  })
 });
 
 
-
-
-// ```md
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
